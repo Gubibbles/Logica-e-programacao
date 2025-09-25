073a929cbe10c9d080c417bbfcef82e69ef41630
Gubibbles <mrsgbbs@hotmail.com>
2025-09-25 14:03:00 -0300

    desafio-aula-13

--- Aula-13-estrutura-condicional/desafio/script.js
+++ Aula-13-estrutura-condicional/desafio/script.js
@@ -3,18 +3,33 @@ let numero2 = window.document.getElementById("num2")
 let operacao = window.document.getElementById("operacao")
 let resultado = window.document.getElementById("resultado")
 
-function somar() {
-    resultado.innerHTML = parseFloat(numero1.value) + parseFloat(numero2.value)
-}
+function calcular() {
+    let num1 = parseFloat(numero1.value);
+    let num2 = parseFloat(numero2.value);
+    let op = operacao.value;
 
-function subtrair() {
-    resultado.innerHTML = parseFloat(numero1.value) - parseFloat(numero2.value)
-}
+    if (isNaN(num1) || isNaN(num2)) {
+        resultado.innerHTML = "Digite números válidos.";
+        return;
+    }
 
-function multiplicar() {
-    resultado.innerHTML = parseFloat(numero1.value) * parseFloat(numero2.value)
-}
-
-function dividir() {
-    resultado.innerHTML = parseFloat(numero1.value) / parseFloat(numero2.value)
+    let resultadoCalculo;
+    
+    if (op === "+") {
+        resultadoCalculo = num1 + num2;
+    } else if (op === "-") {
+        resultadoCalculo = num1 - num2;
+    } else if (op === "*") {
+        resultadoCalculo = num1 * num2;
+    } else if (op === "/") {
+        if (num2 === 0) {
+            resultado.innerHTML = "Não é possível realizar uma divisão por zero(0)."
+            return;
+        }
+        resultadoCalculo = num1 / num2;
+    } else {
+        resultado.innerHTML = "Operação inválida.";
+        return;
+    }
+    resultado.innerHTML = `Resultado: ${resultadoCalculo}`;
 }
