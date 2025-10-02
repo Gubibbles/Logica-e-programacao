81fa2878a53b09510f0a5517c018db4250bc610d
Gubibbles <mrsgbbs@hotmail.com>
2025-10-02 10:42:07 -0300

    atividade a terminar

---
+++ Atividade-02-DOM/script.js
@@ -0,0 +1,8 @@
+let alunos= [];
+
+const formAluno = document.getElementById('formAluno');
+const listaAluno = document.getElementById('listaAlunos');
+const contador = document.getElementById('contador');
+const botaoTodos = document.getElementById('botaoTodos');
+const botaoAprovados = document.getElementById('botaoAprovados');
+const botaoReprovados = document.getElementById('botaoReprovados');
